3eb5428f5362f87ba0e244ab17477a81bfd48595
Pablo Ferrari <pferrari@gmail.com>
2023-12-24 14:51:36 -0300

    Initial code for development api connection
    Related to #18

---
+++ solarec.inverters/src/main/java/tech/renovus/solarec/inverters/branch/solis/SolisInverterService.java
@@ -0,0 +1,18 @@
+package tech.renovus.solarec.inverters.branch.solis;
+
+import java.util.Collection;
+
+import tech.renovus.solarec.inverters.common.InverterCofigurationVo;
+import tech.renovus.solarec.inverters.common.InverterService;
+import tech.renovus.solarec.vo.db.data.ClientVo;
+import tech.renovus.solarec.vo.db.data.GenDataVo;
+
+public class SolisInverterService implements InverterService {
+
+	@Override
+	public Collection<GenDataVo> retrieveData(ClientVo client, InverterCofigurationVo configuration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
