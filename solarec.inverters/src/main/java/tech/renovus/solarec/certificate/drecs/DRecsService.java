d221e5ab4cd01cd6862de2a362d3c333ab8a9ae4
Gustavo <79661942+gustavobert204@users.noreply.github.com>
2024-01-03 15:28:11 -0300

    Related to issue #11 #12 #13
    Created methods to send a post json to the D-REC API
    
    Related to issue #11 #12 #13

--- solarec.inverters/src/main/java/tech/renovus/solarec/certificate/drecs/DRecsService.java
+++ solarec.inverters/src/main/java/tech/renovus/solarec/certificate/drecs/DRecsService.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import tech.renovus.solarec.certificate.drecs.api.auth.Auth;
 import tech.renovus.solarec.certificate.drecs.api.auth.AuthResponse;
+import tech.renovus.solarec.certificate.drecs.api.device.Device;
+import tech.renovus.solarec.certificate.drecs.api.device.DeviceResponse;
 import tech.renovus.solarec.certificate.drecs.api.organization.Organization;
 import tech.renovus.solarec.certificate.drecs.api.organization.OrganizationResponse;
 import tech.renovus.solarec.configuration.RenovusSolarecConfiguration;
 import tech.renovus.solarec.connection.JsonCaller;
 import tech.renovus.solarec.vo.db.data.ClientVo;
+import tech.renovus.solarec.vo.db.data.GeneratorVo;
 
 /**
  * API information: https://api.drecs.org/swagger
@@ -19,14 +22,51 @@ public class DRecsService {
 	private static final String DREC_API_URL			= "https://api.drecs.org/api";
 	private static final String ENDPOINT_AUTH			= DREC_API_URL + "/auth/login";
 	private static final String ENDPOINT_ORGANIZATION	= DREC_API_URL + "/Organization";
+	private static final String ENDPOINT_DEVICE 		= DREC_API_URL + "/device";
 	
 	//--- Protected properties ------------------
 	protected @Autowired RenovusSolarecConfiguration config;
 
 	//--- Private methods -----------------------
 	private Organization createOrganizationFrom(ClientVo clientVo) {
-		// TODO Auto-generated method stub
-		return null;
+		Organization organization = new Organization();
+		organization.withName(clientVo.getCliName())
+			.withAddress(clientVo.getCliNameAddress());
+			//.withZipCode(clientVo.getLocations().iterator().next().) //Ahora si es uno a uno porque es una lista? Que pasara si se cambia
+			//.withCity(clientVo.get)
+			//.withCountry(clientVo.get)
+			//.withOrganizationType(clientVo.)
+			//.withOrgEmail(clientVo.)
+			//.withDocumentIds(null)
+			//.withSignatoryDocumentIds(null)
+			
+		return organization;
+	}
+	
+	private Device createDeviceFrom(GeneratorVo generatorVo) {
+		Device device = new Device();
+		device.withExternalId(generatorVo.getId().toString()); //Confirmar si el ID es el ID solicitado
+			//.withProjectName(generatorVo.)
+			//.withAddress()
+			//.withLatitude()
+			//.withLongitude()
+			//.withCountryCode()
+			//.withFuelCode()
+			//.withDeviceTypeCode()
+			//.withCapacity()
+			//.withCommissioningDate() //Ojo dice date pero no da formato
+			//.withGridInterconnection()
+			//.withOffTaker()
+			//.withImpactStory()
+			//.withImages(null) //Lista de imagenes pasadas a string
+			//.withDeviceDescription(generatorVo.getGenDescription()) //Confirmar si es la descripción solicitada
+			//.withEnergyStorage()
+			//.withEnergyStorageCapacity()
+			//.withQualityLabels()
+			//.withSDGBenefits() //Lista de strings, beneficios?
+			//.withVersion()
+		
+		return device;
 	}
 	
 	//--- Public methods ------------------------
@@ -57,7 +97,14 @@ public class DRecsService {
 	/**
 	 * Doc: https://api.drecs.org/swagger/#/device/DeviceController_create
 	 */
-	public void registerDevice() {
+	public void registerDevice(GeneratorVo generatorVo) {
+		AuthResponse authentication = this.authenticate();
+
+		Device device = this.createDeviceFrom(generatorVo);
+		DeviceResponse response = JsonCaller.bearerPost(ENDPOINT_DEVICE, device, authentication.getAccessToken(),
+				DeviceResponse.class);
+
+		// do somthing with the response
 	}
 
 	/**
---
+++ solarec.inverters/src/main/java/tech/renovus/solarec/certificate/drecs/api/device/Device.java
@@ -0,0 +1,379 @@
+package tech.renovus.solarec.certificate.drecs.api.device;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "externalId", "projectName", "address", "latitude", "longitude", "countryCode", "fuelCode",
+		"deviceTypeCode", "capacity", "commissioningDate", "gridInterconnection", "offTaker", "impactStory", "images",
+		"deviceDescription", "energyStorage", "energyStorageCapacity", "qualityLabels", "SDGBenefits", "version" })
+@Generated("jsonschema2pojo")
+public class Device {
+
+	@JsonProperty("externalId")
+	private String externalId;
+	@JsonProperty("projectName")
+	private String projectName;
+	@JsonProperty("address")
+	private String address;
+	@JsonProperty("latitude")
+	private String latitude;
+	@JsonProperty("longitude")
+	private String longitude;
+	@JsonProperty("countryCode")
+	private String countryCode;
+	@JsonProperty("fuelCode")
+	private String fuelCode;
+	@JsonProperty("deviceTypeCode")
+	private String deviceTypeCode;
+	@JsonProperty("capacity")
+	private Integer capacity;
+	@JsonProperty("commissioningDate")
+	private String commissioningDate;
+	@JsonProperty("gridInterconnection")
+	private Boolean gridInterconnection;
+	@JsonProperty("offTaker")
+	private String offTaker;
+	@JsonProperty("impactStory")
+	private String impactStory;
+	@JsonProperty("images")
+	private List<String> images;
+	@JsonProperty("deviceDescription")
+	private String deviceDescription;
+	@JsonProperty("energyStorage")
+	private Boolean energyStorage;
+	@JsonProperty("energyStorageCapacity")
+	private Integer energyStorageCapacity;
+	@JsonProperty("qualityLabels")
+	private String qualityLabels;
+	@JsonProperty("SDGBenefits")
+	private List<String> sDGBenefits;
+	@JsonProperty("version")
+	private String version;
+	@JsonIgnore
+	private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+	@JsonProperty("externalId")
+	public String getExternalId() {
+		return externalId;
+	}
+
+	@JsonProperty("externalId")
+	public void setExternalId(String externalId) {
+		this.externalId = externalId;
+	}
+
+	public Device withExternalId(String externalId) {
+		this.externalId = externalId;
+		return this;
+	}
+
+	@JsonProperty("projectName")
+	public String getProjectName() {
+		return projectName;
+	}
+
+	@JsonProperty("projectName")
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public Device withProjectName(String projectName) {
+		this.projectName = projectName;
+		return this;
+	}
+
+	@JsonProperty("address")
+	public String getAddress() {
+		return address;
+	}
+
+	@JsonProperty("address")
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Device withAddress(String address) {
+		this.address = address;
+		return this;
+	}
+
+	@JsonProperty("latitude")
+	public String getLatitude() {
+		return latitude;
+	}
+
+	@JsonProperty("latitude")
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public Device withLatitude(String latitude) {
+		this.latitude = latitude;
+		return this;
+	}
+
+	@JsonProperty("longitude")
+	public String getLongitude() {
+		return longitude;
+	}
+
+	@JsonProperty("longitude")
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public Device withLongitude(String longitude) {
+		this.longitude = longitude;
+		return this;
+	}
+
+	@JsonProperty("countryCode")
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	@JsonProperty("countryCode")
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public Device withCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+		return this;
+	}
+
+	@JsonProperty("fuelCode")
+	public String getFuelCode() {
+		return fuelCode;
+	}
+
+	@JsonProperty("fuelCode")
+	public void setFuelCode(String fuelCode) {
+		this.fuelCode = fuelCode;
+	}
+
+	public Device withFuelCode(String fuelCode) {
+		this.fuelCode = fuelCode;
+		return this;
+	}
+
+	@JsonProperty("deviceTypeCode")
+	public String getDeviceTypeCode() {
+		return deviceTypeCode;
+	}
+
+	@JsonProperty("deviceTypeCode")
+	public void setDeviceTypeCode(String deviceTypeCode) {
+		this.deviceTypeCode = deviceTypeCode;
+	}
+
+	public Device withDeviceTypeCode(String deviceTypeCode) {
+		this.deviceTypeCode = deviceTypeCode;
+		return this;
+	}
+
+	@JsonProperty("capacity")
+	public Integer getCapacity() {
+		return capacity;
+	}
+
+	@JsonProperty("capacity")
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+
+	public Device withCapacity(Integer capacity) {
+		this.capacity = capacity;
+		return this;
+	}
+
+	@JsonProperty("commissioningDate")
+	public String getCommissioningDate() {
+		return commissioningDate;
+	}
+
+	@JsonProperty("commissioningDate")
+	public void setCommissioningDate(String commissioningDate) {
+		this.commissioningDate = commissioningDate;
+	}
+
+	public Device withCommissioningDate(String commissioningDate) {
+		this.commissioningDate = commissioningDate;
+		return this;
+	}
+
+	@JsonProperty("gridInterconnection")
+	public Boolean getGridInterconnection() {
+		return gridInterconnection;
+	}
+
+	@JsonProperty("gridInterconnection")
+	public void setGridInterconnection(Boolean gridInterconnection) {
+		this.gridInterconnection = gridInterconnection;
+	}
+
+	public Device withGridInterconnection(Boolean gridInterconnection) {
+		this.gridInterconnection = gridInterconnection;
+		return this;
+	}
+
+	@JsonProperty("offTaker")
+	public String getOffTaker() {
+		return offTaker;
+	}
+
+	@JsonProperty("offTaker")
+	public void setOffTaker(String offTaker) {
+		this.offTaker = offTaker;
+	}
+
+	public Device withOffTaker(String offTaker) {
+		this.offTaker = offTaker;
+		return this;
+	}
+
+	@JsonProperty("impactStory")
+	public String getImpactStory() {
+		return impactStory;
+	}
+
+	@JsonProperty("impactStory")
+	public void setImpactStory(String impactStory) {
+		this.impactStory = impactStory;
+	}
+
+	public Device withImpactStory(String impactStory) {
+		this.impactStory = impactStory;
+		return this;
+	}
+
+	@JsonProperty("images")
+	public List<String> getImages() {
+		return images;
+	}
+
+	@JsonProperty("images")
+	public void setImages(List<String> images) {
+		this.images = images;
+	}
+
+	public Device withImages(List<String> images) {
+		this.images = images;
+		return this;
+	}
+
+	@JsonProperty("deviceDescription")
+	public String getDeviceDescription() {
+		return deviceDescription;
+	}
+
+	@JsonProperty("deviceDescription")
+	public void setDeviceDescription(String deviceDescription) {
+		this.deviceDescription = deviceDescription;
+	}
+
+	public Device withDeviceDescription(String deviceDescription) {
+		this.deviceDescription = deviceDescription;
+		return this;
+	}
+
+	@JsonProperty("energyStorage")
+	public Boolean getEnergyStorage() {
+		return energyStorage;
+	}
+
+	@JsonProperty("energyStorage")
+	public void setEnergyStorage(Boolean energyStorage) {
+		this.energyStorage = energyStorage;
+	}
+
+	public Device withEnergyStorage(Boolean energyStorage) {
+		this.energyStorage = energyStorage;
+		return this;
+	}
+
+	@JsonProperty("energyStorageCapacity")
+	public Integer getEnergyStorageCapacity() {
+		return energyStorageCapacity;
+	}
+
+	@JsonProperty("energyStorageCapacity")
+	public void setEnergyStorageCapacity(Integer energyStorageCapacity) {
+		this.energyStorageCapacity = energyStorageCapacity;
+	}
+
+	public Device withEnergyStorageCapacity(Integer energyStorageCapacity) {
+		this.energyStorageCapacity = energyStorageCapacity;
+		return this;
+	}
+
+	@JsonProperty("qualityLabels")
+	public String getQualityLabels() {
+		return qualityLabels;
+	}
+
+	@JsonProperty("qualityLabels")
+	public void setQualityLabels(String qualityLabels) {
+		this.qualityLabels = qualityLabels;
+	}
+
+	public Device withQualityLabels(String qualityLabels) {
+		this.qualityLabels = qualityLabels;
+		return this;
+	}
+
+	@JsonProperty("SDGBenefits")
+	public List<String> getSDGBenefits() {
+		return sDGBenefits;
+	}
+
+	@JsonProperty("SDGBenefits")
+	public void setSDGBenefits(List<String> sDGBenefits) {
+		this.sDGBenefits = sDGBenefits;
+	}
+
+	public Device withSDGBenefits(List<String> sDGBenefits) {
+		this.sDGBenefits = sDGBenefits;
+		return this;
+	}
+
+	@JsonProperty("version")
+	public String getVersion() {
+		return version;
+	}
+
+	@JsonProperty("version")
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public Device withVersion(String version) {
+		this.version = version;
+		return this;
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+	public Device withAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+		return this;
+	}
+
+}
