84ef46961437494eb9dfc1f91b707d0943be3e54
Pablo Ferrari <pferrari@gmail.com>
2024-01-02 21:20:39 -0300

    Added new settings to store / check for alert configuration
    Releated to #6

--- solarec.vo/src/main/java/tech/renovus/solarec/vo/db/data/CliSettingVo.java
+++ solarec.vo/src/main/java/tech/renovus/solarec/vo/db/data/CliSettingVo.java
@@ -7,14 +7,20 @@ import tech.renovus.solarec.vo.db.relation.DbCliSettingVo;
 public class CliSettingVo extends DbCliSettingVo implements ISetting {
 
 	//--- Public constants ----------------------
-	public static final String FISCAL_YEAR_END_MONTH 	= "fiscalYearEndMonth";
-	public static final String D_RECS_SOLD_PORCENTAGE	= "dRecsSoldPorcentage";
-	public static final String D_RECS_PRICE				= "dRecsPrice";
-	public static final String PREFER_LANGUAGE			= "language";
+	public static final String FISCAL_YEAR_END_MONTH 					= "fiscalYearEndMonth";
+	public static final String D_RECS_SOLD_PORCENTAGE					= "dRecsSoldPorcentage";
+	public static final String D_RECS_PRICE								= "dRecsPrice";
+	public static final String PREFER_LANGUAGE							= "language";
+	public static final String ALERT_DATA_AVAILABILITY_LOWER_THAN		= "alertDataAvailabilityLowerThan";
+	public static final String ALERT_NEGATIVE_CHANGE_EXCEEDING			= "alertNegativeChangeExceeding";
+	public static final String ALERT_TIME_BASED_AVAILABILITY_LOWER_THAN	= "alertTimeBasedAvailabilityLowerThan";
 	
-	public static final String DEFAULT_VALUE_FISCAL_YEAR_END_MONTH	= "12";
-	public static final String DEFAULT_VALUE_D_RECS_SOLD_PORCENTAGE	= "50";
-	public static final String DEFAULT_VALUE_D_RECS_PRICE			= "20";
+	public static final String DEFAULT_VALUE_FISCAL_YEAR_END_MONTH						= "12";
+	public static final String DEFAULT_VALUE_D_RECS_SOLD_PORCENTAGE						= "50";
+	public static final String DEFAULT_VALUE_D_RECS_PRICE								= "20";
+	public static final String DEFAULT_VALUE_ALERT_DATA_AVAILABILITY_LOWER_THAN			= "90";
+	public static final String DEFAULT_VALUE_ALERT_NEGATIVE_CHANGE_EXCEEDING			= "6";
+	public static final String DEFAULT_VALUE_ALERT_TIME_BASED_AVAILABILITY_LOWER_THAN	= "90";
 
 	//--- Constructors --------------------------
 	public CliSettingVo() {
@@ -38,6 +44,9 @@ public class CliSettingVo extends DbCliSettingVo implements ISetting {
 			case D_RECS_SOLD_PORCENTAGE:
 			case D_RECS_PRICE:
 			case PREFER_LANGUAGE:
+			case ALERT_DATA_AVAILABILITY_LOWER_THAN:
+			case ALERT_NEGATIVE_CHANGE_EXCEEDING:
+			case ALERT_TIME_BASED_AVAILABILITY_LOWER_THAN:
 				return true;
 				
 			default:
