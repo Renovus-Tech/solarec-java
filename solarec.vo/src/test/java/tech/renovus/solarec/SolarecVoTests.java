557573b63997c9be14ed04b435de15e95e639f04
Pablo Ferrari <pferrari@gmail.com>
2024-05-22 19:38:52 -0300

    Adjusted testing code and coverage
    Related to #13

--- solarec.vo/src/test/java/tech/renovus/solarec/SolarecVoTests.java
+++ solarec.vo/src/test/java/tech/renovus/solarec/SolarecVoTests.java
@@ -5,10 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import tech.renovus.solarec.vo.comparator.ComparatorTests;
+import tech.renovus.solarec.vo.db.data.BasicVoTester;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	ComparatorTests.class, 
+	BasicVoTester.class
 })
 
 public class SolarecVoTests {
---
+++ solarec.vo/src/test/java/tech/renovus/solarec/vo/db/data/AlertDefinitionVoTest.java
@@ -0,0 +1,35 @@
+package tech.renovus.solarec.vo.db.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AlertDefinitionVoTest {
+
+	@Test
+	public void test() throws Exception {
+		Integer pk1 = Integer.valueOf(1);
+
+		AlertDefinitionVo vo = new AlertDefinitionVo();
+		AlertDefinitionVo vo2 = new AlertDefinitionVo();
+		AlertDefinitionVo voPk = new AlertDefinitionVo(pk1);
+		
+		vo.setAlertDefId(pk1);
+		vo.setChildrensId();
+		vo.synchronize(vo2);
+		vo.synchronizeForce(AlertDefinitionVo.SYNC_INIT);
+		
+		vo2.setPk(vo);
+
+		assertEquals(pk1, voPk.getAlertDefId());
+		assertEquals(voPk, voPk);
+		assertTrue(voPk.isSame(voPk));
+		assertTrue(voPk.validData());
+		
+		assertEquals(voPk.hashCode(), voPk.hashCode());
+		
+		assertEquals(vo,vo2);
+	}
+
+}
