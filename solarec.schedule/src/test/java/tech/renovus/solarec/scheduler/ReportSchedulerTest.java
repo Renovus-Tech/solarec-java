a6c43bc2e3fabf58d5c356949ed98792b34e0545
Pablo Ferrari <pferrari@gmail.com>
2024-10-14 23:19:32 -0300

    Fixed test

--- solarec.schedule/src/test/java/tech/renovus/solarec/scheduler/ReportSchedulerTest.java
+++ solarec.schedule/src/test/java/tech/renovus/solarec/scheduler/ReportSchedulerTest.java
@@ -1,5 +1,12 @@
 package tech.renovus.solarec.scheduler;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class ReportSchedulerTest {
 
+	@Test public void test() {
+		assertTrue(null == null);
+	}
 }
