3ba08f6dc73d48143f36c7d6527a108cf5a4d608
Pablo Ferrari <pferrari@gmail.com>
2025-02-01 08:32:39 -0300

    Mock inverter service testing class

---
+++ solarec.schedule/src/test/java/tech/renovus/solarec/scheduler/InverterServiceTest.java
@@ -0,0 +1,63 @@
+package tech.renovus.solarec.scheduler;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import tech.renovus.solarec.inverters.common.InverterService;
+import tech.renovus.solarec.vo.db.data.CliGenAlertVo;
+import tech.renovus.solarec.vo.db.data.CliLocAlertVo;
+import tech.renovus.solarec.vo.db.data.ClientVo;
+import tech.renovus.solarec.vo.db.data.GenDataVo;
+import tech.renovus.solarec.vo.db.data.StaDataVo;
+
+public class InverterServiceTest implements InverterService {
+
+	public static boolean calledPrepareFor = false;
+	public static boolean calledCanRetrieve = false;
+	public static boolean calledContinueWithStats = false;
+	public static boolean calledGetReasonWhyCantRetrieve = false;
+	public static boolean calledRetrieveData = false;
+	
+	@Override
+	public void prepareFor(ClientVo client) {
+		InverterServiceTest.calledPrepareFor = true;
+	}
+
+	@Override
+	public boolean canRetrieve() {
+		InverterServiceTest.calledCanRetrieve = true;
+		return true;
+	}
+
+	@Override
+	public boolean continueWithStats() {
+		InverterServiceTest.calledContinueWithStats = true;
+		return true;
+	}
+
+	@Override
+	public String getReasonWhyCantRetrieve() {
+		InverterServiceTest.calledGetReasonWhyCantRetrieve = true;
+		return null;
+	}
+
+	@Override
+	public InverterData retrieveData() throws InveterServiceException {
+		InverterServiceTest.calledRetrieveData = true;
+		
+		GenDataVo genDataVo = new GenDataVo();
+		StaDataVo staDataVo = new StaDataVo();
+		CliLocAlertVo locAlertVo = new CliLocAlertVo();
+		CliGenAlertVo genAlertVo = new CliGenAlertVo();
+		
+		Collection<GenDataVo> genData = Arrays.asList(genDataVo);
+		Collection<StaDataVo> staData = Arrays.asList(staDataVo);
+		Collection<CliLocAlertVo> locAlerts = Arrays.asList(locAlertVo);
+		Collection<CliGenAlertVo> getAlerts = Arrays.asList(genAlertVo);
+		
+		InverterData newData = new InverterService.InverterData(genData, staData, locAlerts, getAlerts);
+		
+		return newData;
+	}
+
+}
